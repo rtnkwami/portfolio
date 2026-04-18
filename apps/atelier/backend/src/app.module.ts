00575bd4754684698a1de99a41fb43cf761aa1a8
rtnkwami <nukukwami@outlook.com>
2026-04-18 20:34:41 +0000

    feat(atelier/backend): add and configure mikro-orm

--- apps/atelier/backend/src/app.module.ts
+++ apps/atelier/backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import dbConfig from './database/mikro-orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      ...dbConfig,
+      entities: [],
+      entitiesTs: [],
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
