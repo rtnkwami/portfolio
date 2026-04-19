00f33ea9d7f3415faba678afe8b7c22c0c788731
rtnkwami <nukukwami@outlook.com>
2026-04-19 12:44:17 +0000

    feat(atelier/backend): add env variable validation

--- apps/atelier/backend/src/app.module.ts
+++ apps/atelier/backend/src/app.module.ts
@@ -5,9 +5,12 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import dbConfig from 'src/database/mikro-orm.config';
 import { InventoryModule } from './modules/inventory/inventory.module';
 import { CartModule } from './modules/cart/cart.module';
+import { ConfigModule } from '@nestjs/config';
+import { envValidate } from './env.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate: envValidate }),
     MikroOrmModule.forRoot({
       ...dbConfig,
       entities: [],
---
+++ apps/atelier/backend/src/env.validation.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  PORT: z.coerce.number().min(0).max(65535),
+  DATABASE_URL: z.url({ protocol: /^postgres(ql)?$/ }),
+  ISSUER_BASE_URL: z.url({ protocol: /^https$/ }),
+  REDIS_ENDPOINT: z.url({ protocol: /^redis$/ }),
+});
+
+export function envValidate(envVariables: Record<string, unknown>) {
+  const result = envSchema.safeParse(envVariables);
+
+  if (result.error) {
+    const errors = z.flattenError(result.error);
+    throw new Error(JSON.stringify(errors, null, 2));
+  }
+  return result.data;
+}
