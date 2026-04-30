9f9c4d997da94d73243072e46d022f62f8858ca7
rtnkwami <nukukwami@outlook.com>
2026-04-30 23:43:16 +0000

    feat(scripts): create script to detect changes for containerization

---
+++ scripts/detect-changes.py
@@ -0,0 +1,47 @@
+import subprocess
+import json
+from pathlib import Path
+from os import environ
+
+def find_affected_projects():
+    query = subprocess.run(
+        ["moon", "query", "affected", "--downstream", "deep"],
+        capture_output = True
+    )
+    results = json.loads(query.stdout)
+    
+    affected: list[str] = []
+    for project, _ in results["projects"].items():
+        affected.append(project)
+    
+    return affected
+
+
+def determine_projects_to_containerize(projects: list[str]):
+    # run script using moon cli, otherwise this won't capture the env var
+    workspace_root = environ.get("MOON_WORKSPACE_ROOT")
+    containerize: list[str] = []
+    
+    if workspace_root is None:
+        raise Exception("script is not being run in moon context")
+    
+    app_dir = workspace_root / Path("apps")
+
+    for project in projects:
+        project_dir = app_dir / project
+        
+        if not project_dir.exists():
+            continue
+    
+        containerize.append(project)
+    return containerize
+
+
+def main():
+    affected = find_affected_projects()
+    projects = determine_projects_to_containerize(affected)
+    print(json.dumps(projects))
+
+
+if __name__ == "__main__":
+    main()
