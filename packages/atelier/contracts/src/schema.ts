179721371d0c8fc8bee9f50c8f4d2a105100b5cd
rtnkwami <nukukwami@outlook.com>
2026-05-14 15:59:01 +0000

    feat(inventory): add create product endpoint to api spec

--- packages/atelier/contracts/src/schema.ts
+++ packages/atelier/contracts/src/schema.ts
@@ -52,6 +52,22 @@ export interface paths {
         patch: operations["updateCategory"];
         trace?: never;
     };
+    "/inventory/products": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        post: operations["createProduct"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -82,6 +98,23 @@ export interface components {
             error: string;
             statusCode: number;
             message: string;
+        };
+        CreateProductParams: {
+            product: {
+                name: string;
+                description?: string;
+                price: number;
+                stock: number;
+            };
+            /** Format: uuid */
+            category_id: string;
+        };
+        PrivateProduct_Output: {
+            name: string;
+            description?: string;
+            price: number;
+            category: string;
+            stock: number;
         };
         ValidationErrorDto: {
             statusCode: number;
@@ -275,6 +308,45 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["CategoryResponse_Output"];
+                };
+            };
+            500: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["InternalServerErrorDto"];
+                };
+            };
+        };
+    };
+    createProduct: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["CreateProductParams"];
+            };
+        };
+        responses: {
+            201: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["PrivateProduct_Output"];
+                };
+            };
+            409: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["StandardErrorDto"];
                 };
             };
             500: {
